e5101e983c37ffe9e1b47b47fb442cdae31ef9e3
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-03-18 12:25:46 +0530

    scroll effect

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import "./main.css";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
@@ -12,11 +12,40 @@ function Main() {
     setInput,
     input,
   } = useContext(Context);
+
+
+  const [phase1, setPhase1] = useState("Suggest beautiful places to see on an upcoming road trip")
+  const [phase2, setPhase2] = useState("Breifly sumarize this concept: urban planning")
+  const [phase3, setPhase3] = useState("Brainstorm team bonding activities for our work retreat")
+  const [phase4, setPhase4] = useState("Improve the readability of the following code")
+   const resultRef = useRef(null);
+   useEffect(() => {
+    scrollToBottom();
+  }, [resultData]);
+  const scrollToBottom = () => {
+    if (resultRef.current) {
+      resultRef.current.scrollTop = resultRef.current.scrollHeight;
+    }
+  };
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    const numberOfLines = e.target.value.split('\n').length;
+    setInput(e.target.value);
+    if (numberOfLines === 0) {
+      e.target.rows = 1;
+    } else if (numberOfLines <= 5) {
+      e.target.rows = numberOfLines;
+    } else {
+      e.target.rows = 5;
+    }
+  };
+
+
   return (
     <>
       <div className="main">
         <div className="nav">
-          <p>Gemini</p>
+          <p>Gemini Clone <img src={assets.gemini_icon} alt="" /></p>
           <img src={assets.user_icon} alt="" />
         </div>
         <div className="main-container">
@@ -29,28 +58,28 @@ function Main() {
                 <p>How can i help you today?</p>
               </div>
               <div className="cards">
-                <div className="card">
+                <div className="card" onClick={()=>setInput(phase1)}>
                   <p>
-                    Suggest beautiful places to see on an upcoming road trip
+                    {phase1}
                   </p>
                   <img src={assets.compass_icon} alt="" />
                 </div>
-                <div className="card">
-                  <p>Breifly sumarize this concept: urban planning</p>
+                <div className="card" onClick={()=>setInput(phase2)}>
+                  <p>{phase2}</p>
                   <img src={assets.bulb_icon} alt="" />
                 </div>
-                <div className="card">
-                  <p>Brainstorm team bonding activities for our work retreat</p>
+                <div className="card" onClick={()=>setInput(phase3)}>
+                  <p>{phase3}</p>
                   <img src={assets.message_icon} alt="" />
                 </div>
-                <div className="card">
-                  <p>Improve the readability of the following code</p>
+                <div className="card" onClick={()=>setInput(phase4)}>
+                  <p>{phase4}</p>
                   <img src={assets.code_icon} alt="" />
                 </div>
               </div>
             </>
           ) : (
-            <div className="result">
+            <div className="result" ref={resultRef}>
               <div className="result-title">
                 <img src={assets.user_icon} alt="" />
                 <p>{recentPrompt}</p>
@@ -64,7 +93,7 @@ function Main() {
                     <hr />
                   </div>
                 ) : (
-                  <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                  <p  dangerouslySetInnerHTML={{ __html: resultData }}></p>
                 )}
               </div>
             </div>
@@ -72,10 +101,11 @@ function Main() {
 
           <div className="main-bottom">
             <div className="search-box">
-              <input
-                onChange={(e) => setInput(e.target.value)}
+              <textarea
+                onChange={handleChange}
                 value={input}
                 type="text"
+                rows={1}
                 placeholder="Enter your prompt here..."
               />
 
