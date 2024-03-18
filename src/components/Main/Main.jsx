8f44223da0a8e15805d22664a253dfd6d4dfb90b
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-03-18 11:49:23 +0530

    backup

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./main.css";
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
 function Main() {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
   return (
     <>
       <div className="main">
@@ -10,42 +20,74 @@ function Main() {
           <img src={assets.user_icon} alt="" />
         </div>
         <div className="main-container">
-          <div className="greet">
-            <p>
-              <span>Hello, Dev.</span>
-            </p>
-            <p>How can i help you today?</p>
-          </div>
-          <div className="cards">
-            <div className="card">
-              <p>Suggest beautiful places to see on an upcoming road trip</p>
-              <img src={assets.compass_icon} alt="" />
+          {!showResult ? (
+            <>
+              <div className="greet">
+                <p>
+                  <span>Hello, Dev.</span>
+                </p>
+                <p>How can i help you today?</p>
+              </div>
+              <div className="cards">
+                <div className="card">
+                  <p>
+                    Suggest beautiful places to see on an upcoming road trip
+                  </p>
+                  <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                  <p>Breifly sumarize this concept: urban planning</p>
+                  <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                  <p>Brainstorm team bonding activities for our work retreat</p>
+                  <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                  <p>Improve the readability of the following code</p>
+                  <img src={assets.code_icon} alt="" />
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="result">
+              <div className="result-title">
+                <img src={assets.user_icon} alt="" />
+                <p>{recentPrompt}</p>
+              </div>
+              <div className="result-data">
+                <img src={assets.gemini_icon} alt="" />
+                {loading ? (
+                  <div className="loader">
+                    <hr />
+                    <hr />
+                    <hr />
+                  </div>
+                ) : (
+                  <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                )}
+              </div>
             </div>
-            <div className="card">
-              <p>Breifly sumarize this concept: urban planning</p>
-              <img src={assets.bulb_icon} alt="" />
-            </div>
-            <div className="card">
-              <p>Brainstorm team bonding activities for our work retreat</p>
-              <img src={assets.message_icon} alt="" />
-            </div>
-            <div className="card">
-              <p>Improve the readability of the following code</p>
-              <img src={assets.code_icon} alt="" />
-            </div>
-          </div>
+          )}
+
           <div className="main-bottom">
             <div className="search-box">
-              <input type="text" placeholder="Enter your prompt here..." />
+              <input
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                type="text"
+                placeholder="Enter your prompt here..."
+              />
 
               <div>
                 <img src={assets.gallery_icon} alt="" />
                 <img src={assets.mic_icon} alt="" />
-                <img src={assets.send_icon} alt="" />
+                {input ? <img onClick={() => onSent()} src={assets.send_icon} alt="" />: null}
               </div>
             </div>
             <p className="bottom-info">
-                Gemini may display inaccurate information, including about people, so double check its responses. Your privacy and Gemini apps.
+              Gemini may display inaccurate information, including about people,
+              so double check its responses. Your privacy and Gemini apps.
             </p>
           </div>
         </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,42 +1,57 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./sidebar.css";
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
 function Sidebar() {
+  const [extended, setExtended] = useState(false);
+  const {onSent, prevPrompts, setRecentPrompt, newChat} = useContext(Context);
 
-    const [extended, setExtended] = useState(false);
-
-
+  const loadPrompt = async (prompt) =>{
+    setRecentPrompt(prompt)
+    await onSent(prompt)
+  }
   return (
     <>
       <div className="sidebar">
         <div className="top">
-          <img onClick={()=>setExtended(prev=> !prev)} className="menu" src={assets.menu_icon} alt="" srcset="" />
-          <div className="new-chat">
+          <img
+            onClick={() => setExtended((prev) => !prev)}
+            className="menu"
+            src={assets.menu_icon}
+            alt=""
+            srcset=""
+          />
+          <div onClick={()=>newChat()} className="new-chat">
             <img src={assets.plus_icon} alt="" />
             {extended ? <p>New Chat</p> : null}
           </div>
-          {extended ? <div className="recent">
-                <p className="recent-title">Recent</p>
-                <div className="recent-entry">
-                    <img src={assets.message_icon} alt="" />
-                    <p>What is React? ...</p>
-                </div>
-            </div> : null}
-            
+          {extended ? (
+            <div className="recent">
+              <p className="recent-title">Recent</p>
+              {prevPrompts.map((item, index) => {
+                return (
+                  <div onClick={()=>loadPrompt(item)} className="recent-entry">
+                    <img src={assets.message_icon} alt=""  />
+                    <p>{item.slice(0,20)}...</p>
+                  </div>
+                );
+              })}
+            </div>
+          ) : null}
         </div>
         <div className="bottom">
-            <div className="bottom-item recent-entry">
-                <img src={assets.question_icon} alt="" />
-                {extended ? <p>Help</p> : null}
-            </div>
-            <div className="bottom-item recent-entry">
-                <img src={assets.history_icon} alt="" />
-                {extended ? <p>Activity</p> : null}
-            </div>
-            <div className="bottom-item recent-entry">
-                <img src={assets.setting_icon} alt="" />
-                {extended ? <p>Setting</p> : null}
-            </div>
+          <div className="bottom-item recent-entry">
+            <img src={assets.question_icon} alt="" />
+            {extended ? <p>Help</p> : null}
+          </div>
+          <div className="bottom-item recent-entry">
+            <img src={assets.history_icon} alt="" />
+            {extended ? <p>Activity</p> : null}
+          </div>
+          <div className="bottom-item recent-entry">
+            <img src={assets.setting_icon} alt="" />
+            {extended ? <p>Setting</p> : null}
+          </div>
         </div>
       </div>
     </>
